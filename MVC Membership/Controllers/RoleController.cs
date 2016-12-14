f2b2b83389e04e40c31455283cc8c61c6316a3fb
Brighty28 <abright2012@gmail.com>
2016-12-14 16:34:47 +0000

    Initial check in

---
+++ MVC Membership/Controllers/RoleController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MVC_Membership.Models;
+
+namespace MVC_Membership.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            var role = new IdentityRole();
+            return View(role);
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            try
+            {
+                context.Roles.Add(role);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
